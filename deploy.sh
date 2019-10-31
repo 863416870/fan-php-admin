fed7dbc404bacba68ccf9dd964f608ff11a33b0e
863416870 <863416870@qq.com>
2019-10-31 09:59:45 +0800

    [mod]调整deploy

--- deploy.sh
+++ deploy.sh
@@ -16,7 +16,7 @@
 		git pull
 		git checkout master
 		echo "changing permissions..."
-		chown -R $WEB_USER:$WEB_USERGROUP $WEB_PATH
+		chown -R $WEB_USER:$WEB_USERGROUP $WEB_PATH."/deploy.sh"
 		echo "Finished."
         
 		# echo "start clear cache..."
