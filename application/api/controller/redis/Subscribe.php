850e7fc2050e18e284bb91939037042775ad6426
863416870 <863416870@qq.com>
2019-11-22 18:01:44 +0800

    [mod]

--- application/api/controller/redis/Subscribe.php
+++ application/api/controller/redis/Subscribe.php
@@ -8,7 +8,7 @@
 namespace app\api\controller\redis;
 
 use app\lib\redis\Base as RedisBase;
-use think\Log;
+use think\facade\Log;
 
 //use app\common\tools\Log;
 
@@ -19,7 +19,7 @@ class Subscribe
 
     public function sub()
     {
-        app('log')->write(time()."__超时任务__:");
+        Log::write(time()."__超时任务__:");
 
 
         $redis = new RedisBase();
@@ -27,11 +27,11 @@ class Subscribe
 
         $redis->psubscribe(array('__keyevent@0__:expired'), function ($redis, $pattern, $chan, $msg) {
             //逻辑处理
-            app('log')->write('[1]--过期事件的订阅 ' . json_encode($msg));
-            app('log')->write(json_encode($redis));
-            app('log')->write(json_encode($pattern));
-            app('log')->write(json_encode($chan));
-            app('log')->write($msg);
+            Log::write('[1]--过期事件的订阅 ' . json_encode($msg));
+            Log::write(json_encode($redis));
+            Log::write(json_encode($pattern));
+            Log::write(json_encode($chan));
+            Log::write($msg);
         });
     }
 }
