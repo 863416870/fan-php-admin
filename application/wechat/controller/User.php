24c7c55548c8eeb9d6a10ffb734d021fbeb34dcd
863416870 <863416870@qq.com>
2019-11-27 18:59:50 +0800

    [add]测试小程序

--- application/wechat/controller/User.php
+++ application/wechat/controller/User.php
@@ -17,12 +17,35 @@ class User extends Controller
      * @return array
      * @throws \think\Exception
      */
+
+    public $Crypt;
+    public function __construct()
+    {
+//        const APP_ID = "wx5c418a3419f75d9d";
+//        const APP_SECRET = "a1b9bc964d69111742d36e96b46093e2";
+        $config = [
+            'token'          => 'test',
+            'appid'          => 'wx5c418a3419f75d9d',
+            'appsecret'      => 'a1b9bc964d69111742d36e96b46093e2',
+            'encodingaeskey' => 'BJIUzE0gqlWy0GxfPp4J1oPTBmOrNDIGPNav1YFH5Z5',
+            // 配置商户支付参数（可选，在使用支付功能时需要）
+            'mch_id'         => "1235704602",
+            'mch_key'        => 'IKI4kpHjU94ji3oqre5zYaQMwLHuZPmj',
+            // 配置商户支付双向证书目录（可选，在使用退款|打款|红包时需要）
+            'ssl_key'        => '',
+            'ssl_cer'        => '',
+            // 缓存目录配置（可选，需拥有读写权限）
+            'cache_path'     => '',
+        ];
+        $this->Crypt = new \WeMini\Crypt($config);
+    }
+
     public function login(Request $request)
     {
 
         $params = $request->post();
-
-        Log::write("code".json_encode($params));
+        $info = $this->Crypt->session($params['code']);
+        Log::write("code".json_encode($info));
     }
 
 
