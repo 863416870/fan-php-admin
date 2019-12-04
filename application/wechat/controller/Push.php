46112ebacbdf08c3fcc8c1c592ea678854be9b50
863416870 <863416870@qq.com>
2019-12-04 14:13:43 +0800

    [mod]调整微信认证成功

--- application/wechat/controller/Push.php
+++ application/wechat/controller/Push.php
@@ -26,27 +26,8 @@ class Push extends Controller
     }
 
     public function index(){
-        $timestamp = $_GET['timestamp'];
-        $nonce     = $_GET['nonce'];
-        $token     = 'fanguojie2';
-        $signature = $_GET['signature'];
-        $array     = array($timestamp,$nonce,$token);
-        sort($array);
-        //将排序后的三个参数拼接之后参数拼接之后进行sha1加密
-        $tmpstr    = implode('',$array);
-        $tmpstr    = sha1($tmpstr);
-        //将加密后的字符串与signature进行对比；
-        Log::error("tmpstr------------".$tmpstr);
-        if($tmpstr == $signature && isset($_GET['echostr'])){
-            Log::error($_GET['echostr']);
-            echo $_GET['echostr'];
-            exit;
-        }else{
-            return writeJson(201, [], '成功');
-        }
-
-//        new BasicPushEvent($this->config);
-//        return writeJson(201, [], '成功');
+        new BasicPushEvent($this->config);
+        return writeJson(201, [], '成功');
     }
 
     public function login(){
